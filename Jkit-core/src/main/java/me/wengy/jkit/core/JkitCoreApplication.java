ff607f78fcaddf80bc6a1c7ee57bbfd0ba976593
wengy <xuwenjing_sx@migu.cn>
2019-09-09 19:11:30 +0800

    Add basic component jkit-core

---
+++ Jkit-core/src/main/java/me/wengy/jkit/core/JkitCoreApplication.java
@@ -0,0 +1,13 @@
+package me.wengy.jkit.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkitCoreApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(JkitCoreApplication.class, args);
+    }
+    
+}
